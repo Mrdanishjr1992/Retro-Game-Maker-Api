50bfb39a3f727778b25890521e4a6510269bfb31
mrdanishjr1992 <mrdanishjr1992@gmail.com>
2021-07-05 18:39:21 -0500

    map schema updated

--- models/Map.js
+++ models/Map.js
@@ -1,37 +1,38 @@
 const mongoose = require('mongoose');
 
 const mapSchema = new mongoose.Schema(
-	{
-		tiles: [
-			[
-				{
-					id: Number,
-					x: Number,
-					y: Number,
-					v: {
-						x: Number,
-						y: Number,
-					},
-				},
-			],
-		],
-		bgTile: {
-			id: Number,
-			x: Number,
-			y: Number,
-			v: {
-				x: Number,
-				y: Number,
-			},
-		},
-		mapType: String,
-		size: {
-			width: Number,
-			height: Number,
-		},
-		playerId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-	},
-	{ timestamps: true }
+  {
+    tiles: [
+      [
+        {
+          id: Number,
+          x: Number,
+          y: Number,
+          v: {
+            x: Number,
+            y: Number,
+          },
+          walkable: true,
+        },
+      ],
+    ],
+    bgTile: {
+      id: Number,
+      x: Number,
+      y: Number,
+      v: {
+        x: Number,
+        y: Number,
+      },
+    },
+    mapType: String,
+    size: {
+      width: Number,
+      height: Number,
+    },
+    playerId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  },
+  { timestamps: true },
 );
 
 const Map = mongoose.model('Map', mapSchema);
